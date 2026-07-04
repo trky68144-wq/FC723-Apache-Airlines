0b3aa875b675a49c036bb283d7741992736f3476
trky68144-wq <trky68144@gmail.com>
2026-07-04 22:45:28 +0100

    Initial project setup
    Created apache_airlines.py with required imports

--- Application/Main.py
+++ Application/Main.py
@@ -5,3 +5,11 @@ Created on Sat Jul  4 20:44:34 2026
 @author: PC
 """
 
+# Import random to generate random characters for booking references
+import random
+ 
+# Import string to get the list of letters and digits
+import string
+ 
+# Import sqlite3 to create and use a local database for passenger details
+import sqlite3
